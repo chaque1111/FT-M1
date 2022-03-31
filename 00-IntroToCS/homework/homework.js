ad6098117a279a1e239881ca2d8418b201a4d129
chaque1111 <99604927+chaque1111@users.noreply.github.com>
2022-03-31 15:01:23 -0300

    uwu

--- 00-IntroToCS/homework/homework.js
+++ 00-IntroToCS/homework/homework.js
@@ -3,25 +3,24 @@
 function BinarioADecimal(num) {
   // tu codigo aca
   // convertir
-  let suma = 0;
-  let mult;
-  let invert = num.split("").reverse();
-  for(let i=0; i<invert.length; i++){
-   mult = invert[i] * Math.pow(2,i);
-   suma += mult;
-  }
- return suma; 
+ let suma = 0;
+ 
+ let array= num.split("").reverse();
+ for(let i=0; i<array.length; i++){
+   suma += array[i] * Math.pow(2,i);
+ }
+ return suma;
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
-  
-let binario = [];
-do{
-  binario.unshift(num%2); // para ahorrarse el invertit el arreglo agrego los numeros siguientes al principio del array con unshift
-  num=Math.floor(num/2)
-}while(num!==0)
-return binario.join(""); //se convierte el array en string 
+  let bin = [];
+  do{
+     bin.unshift(num%2);
+     num = Math.floor(num/2)
+  }while(num>0)
+return bin.join("")
+
 }
 
 
